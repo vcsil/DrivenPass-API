8d4b6a1d210158b0409a2a01c7e07ac00f50509c
vcsil <vcsilva@poli.ufrj.br>
2022-09-12 21:05:59 -0300

    fix: uptade for deploy

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { checkAuthMiddleware } from '../middlewares/authMiddleware';
 
 import authRouter from './authRouter';
 import cardRouter from './cardRouter';
@@ -9,6 +10,9 @@ import safeNoteRouter from './safeNoteRouter';
 const router = Router(); 
 
 router.use(authRouter);
+
+router.use(checkAuthMiddleware);
+
 router.use(credentialRouter);
 router.use(safeNoteRouter);
 router.use(cardRouter);
